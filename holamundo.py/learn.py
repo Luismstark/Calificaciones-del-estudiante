f3d25f54617a44774106be132bd6f4585579e554
Luis Stark <luisstark4102000@gmail.com>
2023-01-15 18:58:28 -0300

    learn.py  renombrado

--- holamundo.py/learn.py
+++ holamundo.py/learn.py
@@ -1,16 +1,28 @@
-name = "luis"
-pwd = 1573
+"""aprendiendo a programar en python"""
+#primero aprendere a usar las variables
+name = "luis miguel"
+age = 22
+maried = False
 
-n = str(input("user name..."))
-p = int(input("password..."))
+"""iniciando el primer hols"""
+print(name)
+print(age)
+print(maried)
 
-if n == name: 
-            print("user correct")
-            if p == pwd:
-                       print(" password correct "+ name);
-            else:
-                print("incorect");
+"""condicional id else"""
+if age >= 18:
+    maried = True;
+
 else:
-    print("muy mal")
+    maried = False
 
-                       
+"""
+bucle while
+"""
+while age <= 56:
+    print(age)
+    age += 1
+    
+
+
+
