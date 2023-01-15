be82f85c6ad98bece85fe517e31b457e63904e1e
Luis Stark <luisstark4102000@gmail.com>
2023-01-15 16:31:31 -0300

    modificado recientemente

---
+++ holamundo.py/hola.py
@@ -0,0 +1,16 @@
+name = "luis"
+pwd = 1573
+
+n = str(input("user name..."))
+p = int(input("password..."))
+
+if n == name: 
+            print("user correct")
+            if p == pwd:
+                       print(" password correct "+ name);
+            else:
+                print("incorect");
+else:
+    print("muy mal")
+
+                       
